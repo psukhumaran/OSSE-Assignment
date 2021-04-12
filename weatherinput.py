730c5e2955fe990ea925f264aab5623f86b1cb12
psukhumaran <73813478+psukhumaran@users.noreply.github.com>
2021-04-12 11:54:54 +0530

    Update weatherinput.py

--- weatherinput.py
+++ weatherinput.py
@@ -2,7 +2,7 @@ x = int(input('enter the temperature outside:'))
 if x < 20:
     print('Nice cold weather')
 elif x > 25:
-    print('Nice hot weather')
+    print('Nice and beautiful hot weather')
 else:
     print('it is good outside')
 print('Thank you')
